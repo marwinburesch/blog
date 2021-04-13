84baef481363e1c67be1848e3dd780cc85ee73ab
marwinburesch <marwin@posteo.de>
2021-04-13 12:16:44 +0200

    Add tailwind with base code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 function App() {
-  return <div className="App">CRA & Tailwind</div>;
+  return (
+    <div className="h-screen py-8 bg-gray-200">
+      <div className="p-6 max-w-sm mx-auto rounded-xl bg-white shadow-md flex items-center space-x-4">
+        <h1 className="w-full text-lg  text-center font-bold antialiased">
+          CRA & Tailwind
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
