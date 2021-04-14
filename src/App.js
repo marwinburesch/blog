da82e9ede7d214b0566229ac34af389d7dfe1bc0
marwinburesch <marwin@posteo.de>
2021-04-14 11:56:51 +0200

    Implement fetch from json-server

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import MainContainer from "./components/layout/MainContainer";
 import Post from "./components/Post";
 
 function App() {
+  const [posts, setPosts] = useState();
+
+  useEffect(() => {
+    fetch("http://localhost:3004/posts")
+      .then((response) => response.json())
+      .then((posts) => setPosts(posts));
+  }, []);
+
   return (
     <MainContainer>
       <Header title="One More Blog" />
-      <Post title="Just another post about stuff" />
+      {posts &&
+        posts.map((post) => {
+          return <Post post={post} />;
+        })}
     </MainContainer>
   );
 }
