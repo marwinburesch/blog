1a86fd14eed362bb4bd000e687bf480b996a0b7c
marwinburesch <marwin@posteo.de>
2021-04-14 16:21:49 +0200

    Add edit route

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import MainContainer from "./components/layout/MainContainer";
 import Post from "./components/Post";
+import { Switch, Route, useHistory } from "react-router-dom";
+import EditForm from "./components/pages/EditForm";
 
 function App() {
   const [posts, setPosts] = useState();
+  const history = useHistory();
 
   useEffect(() => {
     fetch("http://localhost:4000/posts")
@@ -12,13 +15,30 @@ function App() {
       .then((posts) => setPosts(posts));
   }, []);
 
+  function handlePostClick(id) {
+    history.push(`/posts/${id}/edit`);
+  }
+
   return (
     <MainContainer>
-      <Header title="One More Blog" />
-      {posts &&
-        posts.map((post) => {
-          return <Post key={post.id} post={post} />;
-        })}
+      <Switch>
+        <Route path="/posts/:id/edit">
+          <EditForm />
+        </Route>
+        <Route exact path="/">
+          <Header title="One More Blog" />
+          {posts &&
+            posts.map((post) => {
+              return (
+                <Post
+                  key={post.id}
+                  post={post}
+                  onClick={() => handlePostClick(post.id)}
+                />
+              );
+            })}
+        </Route>
+      </Switch>
     </MainContainer>
   );
 }
