5f80f17a0e203992fea5bd63c0d632a269c5e641
marwinburesch <marwin@posteo.de>
2021-04-14 15:28:36 +0200

    Format Post

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,8 +2,8 @@ export default function Post({ post }) {
   return (
     <article className="mt-8 px-6 pt-6 pb-2 max-w-md mx-auto rounded-xl bg-green-300 shadow-xl space-x-4 cursor-pointer transform hover:scale-105 transition-transform">
       <div className="flex flex-col">
-        <h1 className="w-full text-lg ">{post.title}</h1>
-        <h2 className="text-right">{post.author}</h2>
+        <h1 className="w-full text-lg font-bold">{post.title}</h1>
+        <h2 className="text-right">{post.createdAt}</h2>
       </div>
     </article>
   );
