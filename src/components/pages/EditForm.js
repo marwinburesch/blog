2efca3222921b8c5e8bb4d2be4c1d0f94a3cf93a
marwinburesch <marwin@posteo.de>
2021-04-14 17:23:32 +0200

    Add patch onSubmit

--- src/components/pages/EditForm.js
+++ src/components/pages/EditForm.js
@@ -19,10 +19,30 @@ export default function EditForm({ postToEdit }) {
     setPost({ ...post, [name]: value });
   }
 
+  function submitForm(event) {
+    event.preventDefault();
+    const stringifiedState = JSON.stringify({
+      title: post.title,
+      body: post.body,
+    });
+    const options = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: stringifiedState,
+    };
+
+    fetch(`http://localhost:4000/posts/${id}`, options)
+      .then((response) => response.json())
+      .then((updatedPost) => console.log(updatedPost))
+      .catch((error) => console.log(error));
+  }
+
   return (
     <>
       <Header title={"Editing Post #" + id} />
-      <form className="flex flex-col items-center">
+      <form onSubmit={submitForm} className="flex flex-col items-center">
         <label htmlFor="title" className="max-w-sm">
           Title
         </label>
@@ -45,6 +65,7 @@ export default function EditForm({ postToEdit }) {
           value={post.body}
           onChange={handleChange}
         />
+        <button type="submit">confirm</button>
       </form>
     </>
   );
