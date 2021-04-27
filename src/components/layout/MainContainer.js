7999d9f720438bda04d8d401a2ec4d2982455068
marwinburesch <marwin@posteo.de>
2021-04-27 11:36:32 +0200

    Update layout

--- src/components/layout/MainContainer.js
+++ src/components/layout/MainContainer.js
@@ -1,5 +1,7 @@
 export default function MainContainer({ children }) {
   return (
-    <div className="container mx-auto h-screen w-full pt-4">{children}</div>
+    <div className="container mx-auto h-screen p-4 overflow-auto text-green-300">
+      {children}
+    </div>
   );
 }
